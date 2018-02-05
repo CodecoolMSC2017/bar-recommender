4a619e6002017ee5dfce48b906c5153d72b8b7f5
Tamas Kiss <sauriaster@gmail.com>
2018-02-05 14:19:42 +0100

    Implemented Value and subclasses, not tested!

---
+++ src/main/java/com/codecool/SingleValue.java
@@ -0,0 +1,16 @@
+package com.codecool;
+
+import java.util.List;
+
+public class SingleValue  extends Value{
+
+    public SingleValue(String param) {
+        super.values.add(param);
+    }
+
+
+
+
+
+
+}
---
+++ src/main/java/com/codecool/Value.java
@@ -0,0 +1,18 @@
+package com.codecool;
+
+import java.util.List;
+
+abstract class Value {
+    List<String> values;
+
+    public Value(List<String> values) {
+        this.values = values;
+    }
+
+    public Value(){}
+
+    public List<String> getInputPattern(){
+        return values;
+    }
+
+}
