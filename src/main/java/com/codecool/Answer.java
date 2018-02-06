186a037d678cb1e5b710d07b265444cf860e1632
Gyorfisti <gyorfisti@gmail.com>
2018-02-06 13:16:59 +0100

    minor refactoring in Answer and Question

--- src/main/java/com/codecool/Answer.java
+++ src/main/java/com/codecool/Answer.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Answer {
     List<Value> values = new ArrayList<Value>();
 
-    public boolean evaulateAnswerByInput(String input){ // incomplete!
+    public boolean evaluateAnswerByInput(String input){ // incomplete!
         for (int i = 0; i < values.size(); i++){
             if (values.get(i).getInputPattern().contains(input)){
                 return true;
