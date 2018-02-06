e2545a29aff2bcc66feb5d41d5e367915a7c6632
Tamas Kiss <sauriaster@gmail.com>
2018-02-06 10:44:32 +0100

    singleValue, Value List to Arraylist, Answer evaulateAnswerByInput updated

--- src/main/java/com/codecool/Value.java
+++ src/main/java/com/codecool/Value.java
@@ -1,18 +1,19 @@
 package com.codecool;
 
+import java.util.ArrayList;
 import java.util.List;
 
 abstract class Value {
-    List<String> values;
+    List<String> valuesList = new ArrayList<String>();
 
     public Value(List<String> values) {
-        this.values = values;
+        this.valuesList = values;
     }
 
     public Value(){}
 
     public List<String> getInputPattern(){
-        return values;
+        return valuesList;
     }
 
 
