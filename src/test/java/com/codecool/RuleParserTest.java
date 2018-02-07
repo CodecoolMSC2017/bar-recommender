d4c78d3e0c8600ae4053f0aaf98c09f6daf13ae4
Gyorfisti <gyorfisti@gmail.com>
2018-02-07 09:57:17 +0100

    added RuleParserTest

---
+++ src/test/java/com/codecool/RuleParserTest.java
@@ -0,0 +1,23 @@
+package com.codecool;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RuleParserTest {
+
+    private RuleParser parser;
+
+    @BeforeEach
+    void setUp() {
+        parser = new RuleParser();
+        parser.loadXmlDocument("src/main/rules.xml");
+    }
+
+    @Test
+    void nullCheck() {
+        RuleRepository repo = parser.getRuleRepository();
+        assertNotEquals(repo, null);
+    }
+}
