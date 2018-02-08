bdca740312cb35ed88b98766c3deb90219503c92
prokajkristof <kiscoloshun@gmail.com>
2018-02-08 09:52:19 +0100

    added selectiontype

--- src/main/java/com/codecool/SingleValue.java
+++ src/main/java/com/codecool/SingleValue.java
@@ -2,8 +2,10 @@ package com.codecool;
 
 public class SingleValue extends Value {
 
-    public SingleValue(String param) {
-        super.valuesList.add(param);
+    public SingleValue(String param, boolean selectionType) {
+        super.setSelectionType(selectionType);
+        super.getInputPattern().add(param);
+
     }
 
 
