79a35b6e31c7aa3b521a91e88a9d603ba33d5bac
prokajkristof <kiscoloshun@gmail.com>
2018-02-08 11:10:16 +0100

    RuleParser handles MultiValue

--- src/main/java/com/codecool/RuleParser.java
+++ src/main/java/com/codecool/RuleParser.java
@@ -2,11 +2,13 @@ package com.codecool;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.util.Arrays;
 
 
 public class RuleParser extends XMLParser {
@@ -34,9 +36,8 @@ public class RuleParser extends XMLParser {
         String id;
         String question;
         Question quest;
-        Answer answer = new Answer();
-        answer.addValue(new SingleValue("true", true));
-        answer.addValue(new SingleValue("false", false ));
+        Answer answer = null;
+
         NodeList nodes = doc.getElementsByTagName("Rule");
         for (int i = 0; i < nodes.getLength(); i++) {
             Element element;
@@ -44,7 +45,18 @@ public class RuleParser extends XMLParser {
             id = element.getAttribute("id");
             NodeList nodeList = element.getChildNodes();
             question = nodeList.item(1).getTextContent();
-
+            NodeList answersList = nodeList.item(3).getChildNodes();
+            answer = new Answer();
+            for(int j=1; j<=3; j+=2) {
+                String param = ( (Element) answersList.item(j)).getAttribute("value");
+                boolean selectionType = Boolean.parseBoolean(((Element) answersList.item(j)).getNodeName());
+                String type = ((Element) answersList.item(j).getChildNodes().item(1)).getNodeName();
+                if(type.equals("SingleValue")) {
+                    answer.addValue(new SingleValue(param, selectionType));
+                } else if(type.equals("MultipleValue")){
+                    answer.addValue(new MultipleValues(Arrays.asList(param.split(",")),selectionType));
+                }
+            }
             quest = new Question(id, question, answer);
             repo.addQuestion(quest);
         }
